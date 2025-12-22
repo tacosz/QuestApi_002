aa0e26789663dc15db897ab71230fd6a583c2be6
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-23 00:38:01 +0700

    variable private retrofit servis

--- app/src/main/java/com/example/pertemuan11pam/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/pertemuan11pam/repositori/ContainerApp.kt
@@ -1,11 +1,13 @@
 package com.example.pertemuan11pam.repositori
 
+import com.example.pertemuan11pam.apiservice.ServiceApiSiswa
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.create
 
 interface ContainerApp {
     val repositoryDataSiswa : RepositoryDataSiswa
@@ -32,4 +34,8 @@ class DefaultContainerApp : ContainerApp{
         )
         .client(klien)
         .build()
+
+    private val retrofitService : ServiceApiSiswa by lazy {
+        retrofit.create(ServiceApiSiswa::class.java)
+    }
 }
