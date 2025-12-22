ec9bc179eaade62ee5f459bff9e10d615b623f0c
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-23 00:22:36 +0700

    variable logging

--- app/src/main/java/com/example/pertemuan11pam/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/pertemuan11pam/repositori/ContainerApp.kt
@@ -1,5 +1,7 @@
 package com.example.pertemuan11pam.repositori
 
+import okhttp3.logging.HttpLoggingInterceptor
+
 interface ContainerApp {
     val repositoryDataSiswa : RepositoryDataSiswa
 }
@@ -7,5 +9,7 @@ interface ContainerApp {
 class DefaultContainerApp : ContainerApp{
     private val baseurl = "http://10.0.2.2/umyTI/"
 
-
+    val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
 }
