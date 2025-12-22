ece6851de09baf5c0e22ca24f29aa1d00d920293
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-23 00:06:52 +0700

    fungsi get dari data siswa

--- app/src/main/java/com/example/pertemuan11pam/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/pertemuan11pam/apiservice/ServiceApiSiswa.kt
@@ -1,8 +1,13 @@
 package com.example.pertemuan11pam.apiservice
 
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ServiceApiSiswa {
     @GET(value = "bacaTeman.php")
     suspend fun getSiswa(): List<DataSiswa>
+
+    @POST(value= "insertTM.php")
+    suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
 }
