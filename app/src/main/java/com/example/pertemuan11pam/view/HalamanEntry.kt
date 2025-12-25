0267c2a0766b0c739d53d1d99f259d416301f342
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-25 19:07:16 +0700

    button pada form tambah siswa

--- app/src/main/java/com/example/pertemuan11pam/view/HalamanEntry.kt
+++ app/src/main/java/com/example/pertemuan11pam/view/HalamanEntry.kt
@@ -6,8 +6,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
@@ -76,5 +79,13 @@ fun EntrySiswaBody(
             onSiswaValueChange = onSiswaValueChange,
             modifier = Modifier.fillMaxWidth()
         )
+        Button(
+            onClick = onSaveClick,
+            enabled = uiStateSiswa.isEntryValid,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(R.string.btn_submit))
+        }
     }
 }
