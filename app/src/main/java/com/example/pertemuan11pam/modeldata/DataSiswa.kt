cde29c35bd3d4276c3728ea78948a512f0145ce1
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-25 15:38:21 +0700

    membuat data class data siswa

--- app/src/main/java/com/example/pertemuan11pam/modeldata/DataSiswa.kt
+++ app/src/main/java/com/example/pertemuan11pam/modeldata/DataSiswa.kt
@@ -1,2 +1,11 @@
 package com.example.pertemuan11pam.modeldata
 
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class DataSiswa(
+    val id : Int,
+    val nama : String,
+    val alamat : String,
+    val telpon : String
+)
