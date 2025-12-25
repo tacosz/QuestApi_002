82bd40f15895cc3fc3f57c7e1d2484346113fac0
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-25 18:52:11 +0700

    top app bar pada halaman entry

--- app/src/main/java/com/example/pertemuan11pam/view/HalamanEntry.kt
+++ app/src/main/java/com/example/pertemuan11pam/view/HalamanEntry.kt
@@ -1,9 +1,15 @@
 package com.example.pertemuan11pam.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pertemuan11pam.uicontroller.route.DestinasiEntry
 import com.example.pertemuan11pam.viewmodel.EntryViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -13,5 +19,17 @@ fun EntrySiswaScreen(
     modifier: Modifier = Modifier,
     viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
-
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiEntry.titleRes),
+                canNavigateBack = true,
+                navigateUp = navigateBack,
+                scrollBehavior = scrollBehavior
+            )
+        }
+    ){  }
 }
