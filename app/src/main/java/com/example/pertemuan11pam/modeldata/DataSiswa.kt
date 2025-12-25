7e543995b1c0732b33485b1b61a1bf875b339461
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-25 15:41:23 +0700

    membuat data class detail siswa

--- app/src/main/java/com/example/pertemuan11pam/modeldata/DataSiswa.kt
+++ app/src/main/java/com/example/pertemuan11pam/modeldata/DataSiswa.kt
@@ -14,3 +14,10 @@ data class UIStateSiswa(
     val detailSiswa: DetailSiswa = DetailSiswa(),
     val isEntryValid: Boolean = false
 )
+
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = "",
+)
