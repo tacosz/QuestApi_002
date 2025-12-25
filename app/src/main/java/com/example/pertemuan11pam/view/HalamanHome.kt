3399a29fb894a507d955cacb563d9b0dafcf1a43
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-25 19:40:04 +0700

    tambah parameter pada navigate to item entry

--- app/src/main/java/com/example/pertemuan11pam/view/HalamanHome.kt
+++ app/src/main/java/com/example/pertemuan11pam/view/HalamanHome.kt
@@ -1,2 +1,10 @@
 package com.example.pertemuan11pam.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+)
