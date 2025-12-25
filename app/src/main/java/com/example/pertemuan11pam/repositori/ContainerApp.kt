bdc5cd08a53b0f094a4de4a51ea6e6c947bdfd1f
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-25 16:08:18 +0700

    interface repositori data siswa

--- app/src/main/java/com/example/pertemuan11pam/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/pertemuan11pam/repositori/ContainerApp.kt
@@ -1,55 +1,9 @@
 package com.example.pertemuan11pam.repositori
 
-import android.app.Application
-import com.example.pertemuan11pam.apiservice.ServiceApiSiswa
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.create
-import kotlin.getValue
+import com.example.pertemuan11pam.modeldata.DataSiswa
 
-interface ContainerApp {
-    val repositoryDataSiswa : RepositoryDataSiswa
+interface RepositoryDataSiswa {
+    suspend fun getDataSiswa() : List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
 }
 
-class DefaultContainerApp : ContainerApp{
-    private val baseurl = "http://10.0.2.2/umyTI/"
-
-    val logging = HttpLoggingInterceptor().apply {
-        level = HttpLoggingInterceptor.Level.BODY
-    }
-    val klien = OkHttpClient.Builder()
-        .addInterceptor(interceptor = logging)
-        .build()
-
-    private val retrofit: Retrofit = Retrofit.Builder()
-        .baseUrl(baseurl)
-        .addConverterFactory(
-            Json {
-                ignoreUnknownKeys = true
-                prettyPrint = true
-                isLenient = true
-            }.asConverterFactory(contentType = "application/json".toMediaType())
-        )
-        .client(klien)
-        .build()
-
-    private val retrofitService : ServiceApiSiswa by lazy {
-        retrofit.create(ServiceApiSiswa::class.java)
-    }
-
-    override val repositoryDataSiswa: RepositoryDataSiswa by lazy {
-        JaringanRepositoryDataSiswa(serviceAppSiswa = retrofitService)
-    }
-}
-
-class AplikasiDataSiswa : Application() {
-    lateinit var container : ContainerApp
-    override fun onCreate() {
-        super.onCreate()
-        this.container = DefaultContainerApp()
-    }
-}
