814462922f7ad9b6c08fc028c0abe1f6769a7878
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-25 19:43:14 +0700

    fungsi home screen

--- app/src/main/java/com/example/pertemuan11pam/view/HalamanHome.kt
+++ app/src/main/java/com/example/pertemuan11pam/view/HalamanHome.kt
@@ -2,11 +2,16 @@ package com.example.pertemuan11pam.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import java.lang.reflect.Modifier
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     navigateToItemEntry: () -> Unit,
     navigateToItemUpdate:(Int) -> Unit,
-
-)
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val scrollBehavior =
+}
