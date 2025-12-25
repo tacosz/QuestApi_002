9f5c878c37ee92e406d362638ba5b8bd92c27f25
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-25 18:30:08 +0700

    fungsi untuk menangani saat ada perubahan pada text input

--- app/src/main/java/com/example/pertemuan11pam/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/pertemuan11pam/viewmodel/EntryViewModel.kt
@@ -13,10 +13,15 @@ class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa):
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
 
+    /* Fungsi untuk memvalidasi input */
     private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa):
             Boolean {
             return with(uiState) {
                 nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
         }
+    }
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
     }
 }
