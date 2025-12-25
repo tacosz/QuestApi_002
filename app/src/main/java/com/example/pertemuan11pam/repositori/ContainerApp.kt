a7113f090f6c4756d742f6e768907f6b6b8e20e7
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-25 16:12:58 +0700

    class jaringan repositori data siswa

--- app/src/main/java/com/example/pertemuan11pam/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/pertemuan11pam/repositori/ContainerApp.kt
@@ -1,9 +1,17 @@
 package com.example.pertemuan11pam.repositori
 
+import com.example.pertemuan11pam.apiservice.ServiceApiSiswa
 import com.example.pertemuan11pam.modeldata.DataSiswa
+import retrofit2.Response
 
 interface RepositoryDataSiswa {
     suspend fun getDataSiswa() : List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
 }
 
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+): RepositoryDataSiswa{
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+}
