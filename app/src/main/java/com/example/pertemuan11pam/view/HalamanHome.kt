53f669d5aeb41c9c6be8a78206ff310c20a4cf35
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-25 19:45:34 +0700

    top bar dan siswa top app bar

--- app/src/main/java/com/example/pertemuan11pam/view/HalamanHome.kt
+++ app/src/main/java/com/example/pertemuan11pam/view/HalamanHome.kt
@@ -1,8 +1,14 @@
 package com.example.pertemuan11pam.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pertemuan11pam.uicontroller.route.DestinasiHome
 import java.lang.reflect.Modifier
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -13,5 +19,16 @@ fun HomeScreen(
     modifier: Modifier = Modifier,
     viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
-    val scrollBehavior =
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiHome.titleRes),
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior
+            )
+        },
+
+    ) {  }
 }
