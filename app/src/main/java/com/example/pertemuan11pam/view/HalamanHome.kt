d2e8fcbb5b344d07bfe8e5e86a0d39ef6e74e5ab
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-25 20:03:04 +0700

    fungsi untuk loading screen

--- app/src/main/java/com/example/pertemuan11pam/view/HalamanHome.kt
+++ app/src/main/java/com/example/pertemuan11pam/view/HalamanHome.kt
@@ -1,8 +1,10 @@
 package com.example.pertemuan11pam.view
 
+import android.media.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -16,7 +18,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pertemuan11pam.uicontroller.route.DestinasiHome
 import com.example.pertemuan11pam.R
@@ -83,4 +87,11 @@ fun HomeBody(
             )
         }
     }
+}
+@Composable
+fun LoadingScreen(modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.string.loading)
+    )
 }
