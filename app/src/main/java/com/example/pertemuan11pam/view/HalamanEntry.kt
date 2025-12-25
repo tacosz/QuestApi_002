6f3f7f45de8f0ff943c1f80e79c405f1e1a409e7
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-25 18:48:31 +0700

    fungsi entry siswa screen pada halaman entry

--- app/src/main/java/com/example/pertemuan11pam/view/HalamanEntry.kt
+++ app/src/main/java/com/example/pertemuan11pam/view/HalamanEntry.kt
@@ -1,2 +1,17 @@
 package com.example.pertemuan11pam.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pertemuan11pam.viewmodel.EntryViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntrySiswaScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+
+}
