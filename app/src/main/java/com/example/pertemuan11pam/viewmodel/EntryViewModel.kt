d661857d0f4c42600413d9c8da05ca95505382e8
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-25 18:28:15 +0700

    fungsi private validasi input pada entry view model

--- app/src/main/java/com/example/pertemuan11pam/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/pertemuan11pam/viewmodel/EntryViewModel.kt
@@ -6,9 +6,17 @@ import com.example.pertemuan11pam.modeldata.UIStateSiswa
 import com.example.pertemuan11pam.repositori.RepositoryDataSiswa
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import com.example.pertemuan11pam.modeldata.DetailSiswa
 
 class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa):
     ViewModel() {
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa):
+            Boolean {
+            return with(uiState) {
+                nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
 }
