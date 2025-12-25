d636efe06121f06713b645e4d737d82b95b2dfdb
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-25 18:25:22 +0700

    variable ui state siswa pada entry view model

--- app/src/main/java/com/example/pertemuan11pam/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/pertemuan11pam/viewmodel/EntryViewModel.kt
@@ -1,4 +1,14 @@
 package com.example.pertemuan11pam.viewmodel
 
-class EntryViewModel {
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan11pam.modeldata.UIStateSiswa
+import com.example.pertemuan11pam.repositori.RepositoryDataSiswa
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
+class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa):
+    ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
 }
