64515a48b6711c6019d62cb8093a09daf5aaf91b
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-26 15:50:52 +0700

    button untuk delete

--- app/src/main/java/com/example/pertemuan11pam/view/HalamanDetail.kt
+++ app/src/main/java/com/example/pertemuan11pam/view/HalamanDetail.kt
@@ -1,6 +1,9 @@
 package com.example.pertemuan11pam.view
 
 import android.graphics.drawable.Icon
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -10,10 +13,16 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SegmentedButtonDefaults.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
@@ -69,5 +78,32 @@ fun DetailSiswaScreen(
                 .padding(innerPadding)
                 .verticalScroll(rememberScrollState())
         )
+    }
+}
+
+@Composable
+private fun BodyDetailDataSiswa(
+    statusUiDetail: StatusUiDetail,
+    onDelete: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Column(
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+        var deleteConfirmationRequest by rememberSaveable { mutableStateOf(false) }
+        when(statusUiDetail){
+            is StatusUiDetail.Success -> DetailDataSiswa(
+                siswa = statusUiDetail.satusiswa,
+                modifier = Modifier.fillMaxWidth()
+            ) else -> {}
+        }
+        OutlinedButton(
+            onClick = {deleteConfirmationRequest = true},
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(R.string.delete))
+        }
     }
 }
