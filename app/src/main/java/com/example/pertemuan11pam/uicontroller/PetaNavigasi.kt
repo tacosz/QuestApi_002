d8b8b8e5a857177947c59a8da345f5cae8535a06
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-26 13:31:43 +0700

    fungsi host navigasi

--- app/src/main/java/com/example/pertemuan11pam/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/pertemuan11pam/uicontroller/PetaNavigasi.kt
@@ -1,12 +1,37 @@
 package com.example.pertemuan11pam.uicontroller
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.example.pertemuan11pam.uicontroller.route.DestinasiEntry
+import com.example.pertemuan11pam.uicontroller.route.DestinasiHome
+import com.example.pertemuan11pam.view.EntrySiswaScreen
+import com.example.pertemuan11pam.view.HomeScreen
 import java.lang.reflect.Modifier
 
 @Composable
 fun DataSiswaApp(navController: NavHostController = rememberNavController(),
                  modifier: Modifier){
     HostNavigasi(navController = navController)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController = navController, startDestination = DestinasiHome.route,
+        modifier = Modifier){
+        composable(DestinasiHome.route){
+            HomeScreen(navigateToItemEntry = {navController.navigate
+                (DestinasiEntry.route)},
+                navigateToItemUpdate = {
+                    navController.navigate("${DestinasiDetail.route}/${it}")
+                })
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = {navController.navigate(DestinasiHome.route)})
+        }
+    }
 }
