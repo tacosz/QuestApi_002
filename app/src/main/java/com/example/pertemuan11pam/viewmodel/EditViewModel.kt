ad8294815fc5e5a5932c804e67d9a3f36b964afa
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-26 14:58:15 +0700

    variable private id siswa dalam edit view model

--- app/src/main/java/com/example/pertemuan11pam/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/pertemuan11pam/viewmodel/EditViewModel.kt
@@ -1,12 +1,25 @@
 package com.example.pertemuan11pam.viewmodel
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pertemuan11pam.modeldata.UIStateSiswa
+import com.example.pertemuan11pam.modeldata.toUiStateSiswa
 import com.example.pertemuan11pam.repositori.RepositoryDataSiswa
+import kotlinx.coroutines.launch
 
 class EditViewModel (savedStateHandle: SavedStateHandle,private  val repositoryDataSiswa: RepositoryDataSiswa): ViewModel(){
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                .toUiStateSiswa(true)
+        }
+    }
 }
