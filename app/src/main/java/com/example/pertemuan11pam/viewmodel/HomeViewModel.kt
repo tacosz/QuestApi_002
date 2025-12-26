3d5d53d81eb48ec6147a27eef724880b823b3823
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-26 13:12:27 +0700

    tampilan sealed status ui siswa

--- app/src/main/java/com/example/pertemuan11pam/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/pertemuan11pam/viewmodel/HomeViewModel.kt
@@ -1,2 +1,9 @@
 package com.example.pertemuan11pam.viewmodel
 
+import com.example.pertemuan11pam.modeldata.DataSiswa
+
+sealed interface StatusUiSiswa{
+    data class Success(val siswa: List<DataSiswa> = listOf()) : StatusUiSiswa
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
+}
