b57e833a57499850304b74cac2d15020c34492b2
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-26 13:27:34 +0700

    fungsi data siswa app

--- app/src/main/java/com/example/pertemuan11pam/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/pertemuan11pam/uicontroller/PetaNavigasi.kt
@@ -1,2 +1,12 @@
 package com.example.pertemuan11pam.uicontroller
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import java.lang.reflect.Modifier
+
+@Composable
+fun DataSiswaApp(navController: NavHostController = rememberNavController(),
+                 modifier: Modifier){
+    HostNavigasi(navController = navController)
+}
