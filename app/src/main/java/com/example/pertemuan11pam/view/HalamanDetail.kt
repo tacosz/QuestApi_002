7482ae49c76a77d6b9aafc805ac92d9a3b2b144e
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-26 15:35:40 +0700

    fungsi detail siswa screen

--- app/src/main/java/com/example/pertemuan11pam/view/HalamanDetail.kt
+++ app/src/main/java/com/example/pertemuan11pam/view/HalamanDetail.kt
@@ -1,2 +1,19 @@
 package com.example.pertemuan11pam.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pertemuan11pam.viewmodel.DetailViewModel
+import com.example.pertemuan11pam.viewmodel.provider.PenyediaViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailSiswaScreen(
+    navigateToEditItem: (Int) -> Unit,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+
+}
