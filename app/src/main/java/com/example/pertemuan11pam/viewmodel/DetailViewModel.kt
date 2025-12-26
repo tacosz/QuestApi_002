06b0b5f838f516951afb6caf18d39b3f497c2e9a
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-26 15:12:12 +0700

    variable private id siswa pada class detail view model

--- app/src/main/java/com/example/pertemuan11pam/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/pertemuan11pam/viewmodel/DetailViewModel.kt
@@ -1,9 +1,23 @@
 package com.example.pertemuan11pam.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.pertemuan11pam.modeldata.DataSiswa
+import com.example.pertemuan11pam.repositori.RepositoryDataSiswa
 
 sealed interface StatusUiDetail {
     data class Success(val satusiswa: DataSiswa): StatusUiDetail
     object Error : StatusUiDetail
     object Loading : StatusUiDetail
+}
+class DetailViewModel (savedStateHandle: SavedStateHandle, private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel(){
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    var statusUiDetail: StatusUiDetail by mutableStateOf(StatusUiDetail.Loading)
+        private set
+    init {
+        getSatuSiswa()
+    }
 }
