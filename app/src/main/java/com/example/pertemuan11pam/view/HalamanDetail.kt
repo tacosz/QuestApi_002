0e309119db3ca557cd033d62f299a30c631f7004
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-26 15:40:51 +0700

    top app bar pada detail siswa screen

--- app/src/main/java/com/example/pertemuan11pam/view/HalamanDetail.kt
+++ app/src/main/java/com/example/pertemuan11pam/view/HalamanDetail.kt
@@ -1,11 +1,24 @@
 package com.example.pertemuan11pam.view
 
+import android.graphics.drawable.Icon
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SegmentedButtonDefaults.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pertemuan11pam.viewmodel.DetailViewModel
+import com.example.pertemuan11pam.viewmodel.StatusUiDetail
 import com.example.pertemuan11pam.viewmodel.provider.PenyediaViewModel
+import com.example.pertemuan11pam.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -15,5 +28,30 @@ fun DetailSiswaScreen(
     modifier: Modifier = Modifier,
     viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
-
+    Scaffold(
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiDetail.titleRes),
+                canNavigateBack = true,
+                navigateUp = navigateBack
+            )
+        },
+        floatingActionButton = {
+            val uiState = viewModel.statusUiDetail
+            FloatingActionButton(
+                onClick = {
+                    when(uiState){ is StatusUiDetail.Success->
+                    navigateToEditItem(uiState.satusiswa.id) else->{}}
+                },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = stringResource(R.string.update),
+                )
+            }
+        },
+        modifier = modifier
+    ) {  }
 }
