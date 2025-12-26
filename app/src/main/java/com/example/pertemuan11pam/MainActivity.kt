b323bfea520459ae4b71ddd853ce64dc76010032
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-26 13:19:28 +0700

    penyesuaian main activity

--- app/src/main/java/com/example/pertemuan11pam/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan11pam/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.pertemuan11pam.modeldata.DataSiswa
 import com.example.pertemuan11pam.ui.theme.Pertemuan11PAMTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Pertemuan11PAMTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
