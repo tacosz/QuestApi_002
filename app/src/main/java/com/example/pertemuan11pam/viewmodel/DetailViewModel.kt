d638d151ad38476f7cec20ccd9904da56dd556ed
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-26 15:08:57 +0700

    interface dilindungi unt status ui detail

--- app/src/main/java/com/example/pertemuan11pam/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/pertemuan11pam/viewmodel/DetailViewModel.kt
@@ -1,2 +1,9 @@
 package com.example.pertemuan11pam.viewmodel
 
+import com.example.pertemuan11pam.modeldata.DataSiswa
+
+sealed interface StatusUiDetail {
+    data class Success(val satusiswa: DataSiswa): StatusUiDetail
+    object Error : StatusUiDetail
+    object Loading : StatusUiDetail
+}
