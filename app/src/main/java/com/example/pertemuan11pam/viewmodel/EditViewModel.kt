ad09ca6ca50a3d2c8008e506318862ecd45c66fe
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-26 14:56:03 +0700

    variable ui state siswa pada edit view model

--- app/src/main/java/com/example/pertemuan11pam/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/pertemuan11pam/viewmodel/EditViewModel.kt
@@ -1,2 +1,12 @@
 package com.example.pertemuan11pam.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan11pam.modeldata.UIStateSiswa
+import com.example.pertemuan11pam.repositori.RepositoryDataSiswa
+
+class EditViewModel (savedStateHandle: SavedStateHandle,private  val repositoryDataSiswa: RepositoryDataSiswa): ViewModel(){
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+}
