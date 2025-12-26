6252ce2d09cb927a81da7135af4faddcd42d6b1b
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-26 13:21:02 +0700

    fungsi creation extra

--- app/src/main/java/com/example/pertemuan11pam/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pertemuan11pam/viewmodel/provider/PenyediaViewModel.kt
@@ -1,2 +1,10 @@
 package com.example.pertemuan11pam.viewmodel.provider
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import com.example.pertemuan11pam.repositori.AplikasiDataSiswa
+
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
+        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
+        AplikasiDataSiswa
+        )
