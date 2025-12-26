6cba8051cab05cdee35e4a86682acf7a5de478a8
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-12-26 16:36:07 +0700

    object destinasi detail

--- app/src/main/java/com/example/pertemuan11pam/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/pertemuan11pam/uicontroller/route/DestinasiDetail.kt
@@ -1,2 +1,11 @@
 package com.example.pertemuan11pam.uicontroller.route
 
+import com.example.pertemuan11pam.R
+
+object DestinasiDetail: DestinasiNavigasi{
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
